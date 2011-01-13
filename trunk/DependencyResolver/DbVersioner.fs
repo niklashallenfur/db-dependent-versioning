fe3087d52b9c4e4ca4ad5a7dd251096abfd14e5c
hallenfur@gmail.com <hallenfur@gmail.com@d42d6503-aaa6-9ffe-1e4e-bd70a5f41754>
2011-01-13 15:01:44 +0000

    Enabled downgrading of db

--- trunk/DependencyResolver/DbVersioner.fs
+++ trunk/DependencyResolver/DbVersioner.fs
@@ -35,7 +35,7 @@ type DbVersioner(connectionCreator : IConnectionResourceProvider, scriptReposito
         match items with
         |[] -> []
         |scriptSpec::tail -> {scriptSpec with DependentOn = previous}::(ReplaceDependencies (Some(scriptSpec.Name)) tail)
-
+                        
     let DependencyNameList allScripts scriptName =
         let findScript name = List.find (fun x -> x.Name = name) allScripts
         let rec dnl scriptName dependencies =
@@ -71,6 +71,85 @@ type DbVersioner(connectionCreator : IConnectionResourceProvider, scriptReposito
             Some(item)
         Seq.fold folder None sequence
 
+    member x.DownGrade(belowVersion, connection : IConnectionResource) =
+        logger.LogMessage(sprintf "Trying to downgrade below %s " belowVersion, LogImportance.High)
+        let scriptName = ScriptName.Parse(belowVersion)
+        logger.LogMessage("Getting available scripts ", LogImportance.Low)
+        let scripts = scriptRepository.GetAvailableScripts() |> List.ofSeq
+        logger.LogMessage(sprintf "Number of available scripts: %i" (List.length scripts), LogImportance.Low)
+        logger.LogMessage("Getting already executed scripts", LogImportance.Low)
+        let alreadyExecuted = connection.GetAlreadyExecuted()
+
+        let rec buildToExecute name acc existing =
+            match existing with
+            |[] -> ([], false)
+            |x::tail when x = name -> (x::acc, true)
+            |x::tail -> buildToExecute name (x::acc) tail
+            
+        let (namesToExecute, nameFound) = buildToExecute scriptName [] (List.ofSeq alreadyExecuted |> List.rev)        
+        
+        match nameFound with
+        |false -> ignore 0
+        |true ->    let findScript name = List.find (fun x -> x.Name = name) scripts
+                    let scriptsToExecute = List.map findScript namesToExecute |> List.rev          
+                    let undo() = scriptsToExecute |> List.map (fun s -> undoScript connection scriptRepository.LoadScript s) |> ignore                        
+                    logger.LogMessage("Undoing scripts", LogImportance.Medium)
+                    undo()
+
+    member x.Upgrade (testUndo, toVersion, connection : IConnectionResource) =
+        logger.LogMessage(sprintf "Trying to upgrade to %s " toVersion, LogImportance.High)
+        let scriptName = ScriptName.Parse(toVersion)
+        logger.LogMessage("Getting available scripts ", LogImportance.Low)
+        let scripts = scriptRepository.GetAvailableScripts() |> List.ofSeq
+        logger.LogMessage(sprintf "Number of available scripts: %i" (List.length scripts), LogImportance.Low)
+
+        let nameSorted = List.sort scripts
+  
+        let dependent = TransformToItemDependent nameSorted
+        let dependencySorted = List.sortWith (DependencyCompare dependent) dependent
+        
+        logger.LogMessage("Getting already executed scripts", LogImportance.Low)
+        let alreadyExecuted = connection.GetAlreadyExecuted()
+        let lastExecuted = lastItem alreadyExecuted
+
+        logger.LogMessage(sprintf "Number of already executed scripts: %i" (Seq.length alreadyExecuted), LogImportance.Low)
+        match lastExecuted with
+        |Some(x) -> logger.LogMessage(sprintf "Last executed script is %s" (x.ToString()), LogImportance.Medium)
+        |_ -> ignore 0
+    
+        let notExecuted = dependencySorted |> List.filter (fun script -> not (Seq.exists (fun existingScriptName -> script.Name = existingScriptName) alreadyExecuted))
+        let notExecuted = ReplaceDependencies lastExecuted notExecuted
+        
+        logger.LogMessage(sprintf "not executed: %i" (List.length notExecuted),LogImportance.Low)
+
+        let rec buildToExecute name acc existing =
+            match existing with
+            |[] -> []
+            |x::tail when x.Name = name -> x::acc
+            |x::tail -> buildToExecute name (x::acc) tail
+            
+        let scriptsToExecute = buildToExecute scriptName [] notExecuted |> List.rev
+
+        logger.LogMessage(sprintf "Number of scripts to execute: %i" (List.length scriptsToExecute), LogImportance.Medium)
+
+        let apply() = scriptsToExecute |> List.map (fun s -> applyScript connection scriptRepository.LoadScript s) |> ignore
+        let undo() = scriptsToExecute |> List.rev |> List.map (fun s -> undoScript connection scriptRepository.LoadScript s) |> ignore
+        let testUndoScripts() =
+            logger.LogMessage("Undoing scripts", LogImportance.Medium)
+            undo()
+            logger.LogMessage("Re-applying scripts", LogImportance.Medium)
+            apply()
+        
+        logger.LogMessage("Applying scripts", LogImportance.Medium)
+        apply()
+        if testUndo then testUndoScripts()
+
+    member x.DownGradeUpGrade(testUndo, belowVersion, toVersion) =
+        use connection = connectionCreator.CreateConnection()
+        x.DownGrade(belowVersion, connection)
+        x.Upgrade(testUndo, toVersion, connection)
+        connection.Commit()
+
     member x.ApplyLatestScripts (testUndo) =
         logger.LogMessage("Getting available scripts ", LogImportance.Low)
         let scripts = scriptRepository.GetAvailableScripts() |> List.ofSeq
@@ -95,7 +174,7 @@ type DbVersioner(connectionCreator : IConnectionResourceProvider, scriptReposito
         let scriptsToExecute = dependencySorted |> List.filter (fun script -> not (Seq.exists (fun existingScriptName -> script.Name = existingScriptName) alreadyExecuted))
         let scriptsToExecute = ReplaceDependencies lastExecuted scriptsToExecute
 
-        logger.LogMessage(sprintf "Number of scripts to execute: %i" (List.length scriptsToExecute), LogImportance.Medium)        
+        logger.LogMessage(sprintf "Number of scripts to execute: %i" (List.length scriptsToExecute), LogImportance.Medium)
 
         let apply() = scriptsToExecute |> List.map (fun s -> applyScript connection scriptRepository.LoadScript s) |> ignore
         let undo() = scriptsToExecute |> List.rev |> List.map (fun s -> undoScript connection scriptRepository.LoadScript s) |> ignore
@@ -112,3 +191,110 @@ type DbVersioner(connectionCreator : IConnectionResourceProvider, scriptReposito
         logger.LogMessage("Committing changes", LogImportance.Low)
         connection.Commit()
         logger.LogMessage("Done committing changes", LogImportance.Low)
+
+
+open NUnit.Framework
+open Rhino.Mocks
+open MockExtensions
+
+[<TestFixture>]
+type DbVersionerSpecs() =
+
+    let tempColor color = let oldCol = Console.ForegroundColor
+                          Console.ForegroundColor <- color
+                          {new IDisposable with member this.Dispose() = Console.ForegroundColor <- oldCol}
+
+
+
+    let consoleLogger = {new ILogger with 
+                  member this.LogMessage(text, importance) =    let col =   match importance with
+                                                                            |LogImportance.High -> ConsoleColor.White
+                                                                            |LogImportance.Medium -> ConsoleColor.Gray
+                                                                            |LogImportance.Low -> ConsoleColor.DarkGray
+                                                                            |x -> failwithf "%A not implemented" x
+                                                                use c = tempColor col
+                                                                Console.WriteLine(text);
+                  member this.LogError(text) =  use c = tempColor ConsoleColor.Red
+                                                Console.WriteLine(text)}
+
+
+    let mocks = MockRepository()
+
+    let connectionProvider = mocks.Stub<IConnectionResourceProvider>()
+    let connection = mocks.Stub<IConnectionResource>()
+    let scriptRepository = mocks.Stub<IScriptRepository>()
+    let logger = consoleLogger
+    let versioner = DbVersioner(connectionProvider, scriptRepository, logger)
+
+    let moduleName = [1;10]
+    let templateName = {Module = moduleName; Number = -1}
+    let scriptName1 = {templateName with Number = 1}
+    let scriptName2 = {templateName with Number = 2}
+    let scriptName3 = {templateName with Number = 3}
+    let scriptName4 = {templateName with Number = 4}
+
+    let scriptTemplate = {Name = templateName; Path=""; Description=""; DependentOn = None}
+    let script1 = {scriptTemplate with Name = scriptName1}
+    let script2 = {scriptTemplate with Name = scriptName2; DependentOn = Some(scriptName1)}
+    let script3 = {scriptTemplate with Name = scriptName3; DependentOn = Some(scriptName2)}
+    let script4 = {scriptTemplate with Name = scriptName4; DependentOn = Some(scriptName3)}
+
+    let text1 = {ApplyScript="a1"; UndoScript="u1"}
+    let text2 = {ApplyScript="a2"; UndoScript="u2"}
+    let text3 = {ApplyScript="a3"; UndoScript="u3"}
+    let text4 = {ApplyScript="a4"; UndoScript="u4"}
+
+    [<SetUp>]
+    member x.Setup() =
+        mocks.BackToRecordAll()
+        Expect.Call(connectionProvider.CreateConnection()).Return(connection).Repeat.Any() |> ignore        
+        ignore 0
+
+
+    [<Test>]
+    member x.DownGrade_VersionExists() =
+        let alreadyExecuted = [scriptName1;scriptName2;scriptName3;scriptName4]
+        let availableScripts = [script1;script2;script3;script4]
+
+        Expect.Call(connection.GetAlreadyExecuted()).Return(alreadyExecuted) |> ignore
+        Expect.Call(scriptRepository.GetAvailableScripts()).Return(availableScripts) |> ignore
+        
+        Expect.Call(scriptRepository.LoadScript(script3)).Return(text3) |> ignore
+        Expect.Call(scriptRepository.LoadScript(script4)).Return(text4) |> ignore
+        
+        connection.ExecuteScript(text4.UndoScript, Some("Undoing 1.10.4"))
+        connection.UnRegisterExecuted script4
+
+        connection.ExecuteScript(text3.UndoScript, Some("Undoing 1.10.3"))
+        connection.UnRegisterExecuted script3
+
+        mocks.ReplayAll()
+        versioner.DownGrade("1.10.2", connection)
+
+        mocks.VerifyAll()
+        ignore 0
+
+
+    [<Test>]
+    member x.Upgrade_VersionExists() =
+        let alreadyExecuted = [scriptName1;scriptName3;]
+        let availableScripts = [script1;script2;script3;script4]
+
+        Expect.Call(connection.GetAlreadyExecuted()).Return(alreadyExecuted) |> ignore
+        Expect.Call(scriptRepository.GetAvailableScripts()).Return(availableScripts) |> ignore
+        
+        Expect.Call(scriptRepository.LoadScript(script2)).Return(text2) |> ignore
+        Expect.Call(scriptRepository.LoadScript(script4)).Return(text4) |> ignore
+        
+        connection.ExecuteScript(text2.ApplyScript, Some("Applying 1.10.2"))
+        connection.UnRegisterExecuted script2
+
+        connection.ExecuteScript(text4.ApplyScript, Some("Applying 1.10.4"))
+        connection.UnRegisterExecuted script4
+
+        mocks.ReplayAll()
+        versioner.Upgrade(false, "1.10.4", connection)
+
+        mocks.VerifyAll()
+        ignore 0
+
